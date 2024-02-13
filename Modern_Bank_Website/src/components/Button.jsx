56337e1a799e9ba4ebe0c029ebfa4ab426fc3fd9
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-02-13 03:04:51 +0100

    i have added the roundness of 10px to the button component

--- Modern_Bank_Website/src/components/Button.jsx
+++ Modern_Bank_Website/src/components/Button.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line react/prop-types
 const Button = ({styles}) => {
     return (
-        <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}` }>Get Started</button>
+        <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]` }>Get Started</button>
   )
 }
 
--- Modern_Bank_Website/src/components/CardDeal.jsx
+++ Modern_Bank_Website/src/components/CardDeal.jsx
@@ -1,6 +1,19 @@
+import { card } from "../assets"
+import styles, { layout } from "../style"
+import Button from "./Button"
 const CardDeal = () => {
   return (
-    <div>CardDeal</div>
+    <section id="" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Find a better card deal <br className="sm:block hidden" /> in few easy steps.</h2>
+        
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, ducimus quasi maiores voluptatibus similique voluptate  aspernatur error quaerat aliquid optio nam facilis   beatae?</p>
+
+      <Button styles="mt-10" />
+
+      </div>
+
+    </section>
   )
 }
 
