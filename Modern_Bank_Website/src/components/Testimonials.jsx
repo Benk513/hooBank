ac25f7cf5a435b8c753b26d0e7adab8985fb4204
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-02-25 03:43:09 +0100

    i have completed the client testimonial section successfully now i move to the CTA section

--- Modern_Bank_Website/src/components/Testimonials.jsx
+++ Modern_Bank_Website/src/components/Testimonials.jsx
@@ -7,7 +7,7 @@ const Testimonials = () => {
   return (
     <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
       {/* todo */}
-      <div />
+      <div className="absolute-z-[2] w-[60%] h-[60%] rounded-full blue__gradient right-0"  />
       
       <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
         <h1 className={styles.heading2}>what people are <br className="sm:block hidden" /> saying about us</h1>
